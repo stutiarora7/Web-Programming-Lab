15c3175670acd11b51a432aa784580d191d56585
Stuti Arora <67429440+stutiarora7@users.noreply.github.com>
2020-12-02 18:37:27 +0530

    Add files via upload

---
+++ Connections/CartItems.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+    $username = "stuti";
+    $password = "";
+    $dbname="windowshop";
+
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * from cart";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['product_name'] . "</td>";
+            echo "<td>" . $row['price'] . "</td>";
+            echo "<td ><button class=\"submit-button\" type=\"submit\" value=\"button\" onclick=\"removeFromCart('". $row['product_name'] ."')\">
+            Remove</button></td>";
+            echo "</tr>";
+        }
+    }
+    $conn->close();
+?>
---
+++ Connections/RemoveItem.php
@@ -0,0 +1,23 @@
+<?php
+  header('Content-Type: application/json');
+   $servername = "localhost";
+    $username = "stuti";
+    $password = "";
+    $dbname="windowshop";
+
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "DELETE FROM cart WHERE product_name='".$_POST['name']."';";
+    $result = $conn->query($sql);
+
+    if ($result === TRUE) {
+        echo "Record deleted successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+    $conn->close();
+?>
